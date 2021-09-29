93bd241f6db445af848c862d560e6ea4d5eaa86f
selvia1991 <91614652+selvia1991@users.noreply.github.com>
2021-09-29 23:10:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
-from flask import Flask,render_template,request
-import pickle
-import re
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-import pickle
-
-###Loading model and cv
-cv = pickle.load(open('cv.pkl','rb'))
-model = pickle.load(open('review.pkl','rb'))
+model = tf.keras.models.load_model('model.h5')
 
 app = Flask(__name__)
 
@@ -20,19 +9,25 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     if request.method=='POST':
-        new_review = request.form['review']
-        new_review = re.sub('[^a-zA-Z]', ' ', new_review)
-        new_review = new_review.lower()
-        new_review = new_review.split()
-        ps = PorterStemmer()
-        all_stopwords = stopwords.words('english')
-        all_stopwords.remove('not')
-        new_review = [ps.stem(word) for word in new_review if not word in set(all_stopwords)]
-        new_review = ' '.join(new_review)
-        new_corpus = [new_review]
-        new_X_test = cv.transform(new_corpus).toarray()
-        pred = model.predict(new_X_test)
-        return render_template('result.html',prediction=pred)
+        otherData = pd.DataFrame()
+        otherData['text'] = ['review']
+        otherData['text_clean'] = otherData['text'].apply(cleaningText)
+        otherData['text_clean'] = otherData['text_clean'].apply(casefoldingText)
+        otherData.drop(['text'], axis = 1, inplace = True)
+
+        otherData['text_preprocessed'] = otherData['text_clean'].apply(tokenizingText)
+        otherData['text_preprocessed'] = otherData['text_preprocessed'].apply(filteringText)
+        otherData['text_preprocessed'] = otherData['text_preprocessed'].apply(stemmingText)
+        
+        X_otherData = otherData['text_preprocessed'].apply(toSentence)
+        X_otherData = tokenizer.texts_to_sequences(X_otherData.values)
+        X_otherData = pad_sequences(X_otherData, maxlen = X.shape[1])
+
+        y_pred_otherData = model.predict(X_otherData)
+        otherData['Result Prediction'] = y_pred_otherData
+
+
+        return render_template('result.html',prediction=otherData)
 
 if __name__ == "__main__":
-    app.run(debug=True)    
+    app.run(debug=True)  
