89af2319fcaf0487bca0cf00cf7eb19d04a952da
selvia1991 <91614652+selvia1991@users.noreply.github.com>
2021-10-02 13:53:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -60,46 +60,49 @@ def toSentence(list_words): # Convert list of words into sentence
 @app.route('/predict',methods=['POST'])
 def predict():
     if request.method=='POST':
-#         otherData = pd.DataFrame()
+       
 #         otherData['text'] = ['PPKM diperpanjang saja, enak WFH',
 #                      'PPKM diakhiri saja, tidak bisa jalan jalan ke Malang']
+        otherData = pd.DataFrame()
+        otherData['text'] = request.form['review']
+        otherData['text_clean'] = otherData['text'].apply(cleaningText)
+        otherData['text_clean'] = otherData['text_clean'].apply(casefoldingText)
+        otherData.drop(['text'], axis = 1, inplace = True)
 
-#         otherData['text_clean'] = otherData['text'].apply(cleaningText)
-#         otherData['text_clean'] = otherData['text_clean'].apply(casefoldingText)
-#         otherData.drop(['text'], axis = 1, inplace = True)
+        otherData['text_preprocessed'] = otherData['text_clean'].apply(tokenizingText)
+        otherData['text_preprocessed'] = otherData['text_preprocessed'].apply(filteringText)
+        otherData['text_preprocessed'] = otherData['text_preprocessed'].apply(stemmingText)
+        otherData
 
-#         otherData['text_preprocessed'] = otherData['text_clean'].apply(tokenizingText)
-#         otherData['text_preprocessed'] = otherData['text_preprocessed'].apply(filteringText)
-#         otherData['text_preprocessed'] = otherData['text_preprocessed'].apply(stemmingText)
-#         otherData
+        X_otherData = otherData['text_preprocessed'].apply(toSentence)
+        X_otherData = tokenizer.texts_to_sequences(X_otherData.values)
+        X_otherData = pad_sequences(X_otherData, maxlen = X.shape[1])
+        X_otherData
 
-#         X_otherData = otherData['text_preprocessed'].apply(toSentence)
-#         X_otherData = tokenizer.texts_to_sequences(X_otherData.values)
-#         X_otherData = pad_sequences(X_otherData, maxlen = X.shape[1])
-#         X_otherData
+        y_pred_otherData = model.predict(X_otherData)
+        pred = model.predict(y_pred_otherData)
 
-#         y_pred_otherData = model.predict(X_otherData)
+
 #         otherData['Result Prediction'] = y_pred_otherData
-
 #         polarity_decode = {0 : 'Negative', 1 : 'Neutral', 2 : 'Positive'}
 #         otherData['Result Prediction'] = otherData['Result Prediction'].map(polarity_decode)
 #         otherData
         
         
         
-        new_review = request.form['review']
-        new_review = re.sub('[^a-zA-Z]', ' ', new_review)
-        new_review = new_review.lower()
-        new_review = new_review.split()
-        ps = PorterStemmer()
-        all_stopwords = stopwords.words('english')
-        all_stopwords.remove('not')
-        new_review = [ps.stem(word) for word in new_review if not word in set(all_stopwords)]
-        new_review = ' '.join(new_review)
-        new_corpus = [new_review]
-        new_X_test = cv.transform(new_corpus).toarray()
-        pred = model.predict(new_X_test)
-        return render_template('result.html',prediction=pred)
+#         new_review = request.form['review']
+#         new_review = re.sub('[^a-zA-Z]', ' ', new_review)
+#         new_review = new_review.lower()
+#         new_review = new_review.split()
+#         ps = PorterStemmer()
+#         all_stopwords = stopwords.words('english')
+#         all_stopwords.remove('not')
+#         new_review = [ps.stem(word) for word in new_review if not word in set(all_stopwords)]
+#         new_review = ' '.join(new_review)
+#         new_corpus = [new_review]
+#         new_X_test = cv.transform(new_corpus).toarray()
+#         pred = model.predict(new_X_test)
+          return render_template('result.html',prediction=pred)
 
 if __name__ == "__main__":
     app.run(debug=True)    
