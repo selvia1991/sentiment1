66a551d00673f949b56e65af29c1c94decc3a686
selvia1991 <91614652+selvia1991@users.noreply.github.com>
2021-10-02 19:21:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,45 +20,45 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-def cleaningText(text):
-    text = re.sub(r'@[A-Za-z0-9]+', '', text) # remove mentions
-    text = re.sub(r'#[A-Za-z0-9]+', '', text) # remove hashtag
-    text = re.sub(r'RT[\s]', '', text) # remove RT
-    text = re.sub(r"http\S+", '', text) # remove link
-    text = re.sub(r'[0-9]+', '', text) # remove numbers
-    text = re.sub(r"xfxfxxb", '', text) # remove numbers
+# def cleaningText(text):
+#     text = re.sub(r'@[A-Za-z0-9]+', '', text) # remove mentions
+#     text = re.sub(r'#[A-Za-z0-9]+', '', text) # remove hashtag
+#     text = re.sub(r'RT[\s]', '', text) # remove RT
+#     text = re.sub(r"http\S+", '', text) # remove link
+#     text = re.sub(r'[0-9]+', '', text) # remove numbers
+#     text = re.sub(r"xfxfxxb", '', text) # remove numbers
 
-    text = text.replace('\n', ' ') # replace new line into space
-    text = text.translate(str.maketrans('', '', string.punctuation)) # remove all punctuations
-    text = text.strip(' ') # remove characters space from both left and right text
-    return text
+#     text = text.replace('\n', ' ') # replace new line into space
+#     text = text.translate(str.maketrans('', '', string.punctuation)) # remove all punctuations
+#     text = text.strip(' ') # remove characters space from both left and right text
+#     return text
 
-def casefoldingText(text): # Converting all the characters in a text into lower case
-    text = text.lower() 
-    return text
+# def casefoldingText(text): # Converting all the characters in a text into lower case
+#     text = text.lower() 
+#     return text
 
-def tokenizingText(text): # Tokenizing or splitting a string, text into a list of tokens
-    text = word_tokenize(text) 
-    return text
+# def tokenizingText(text): # Tokenizing or splitting a string, text into a list of tokens
+#     text = word_tokenize(text) 
+#     return text
 
-def filteringText(text): # Remove stopwors in a text
-    listStopwords = set(stopwords.words('indonesian'))
-    filtered = []
-    for txt in text:
-        if txt not in listStopwords:
-            filtered.append(txt)
-    text = filtered 
-    return text
+# def filteringText(text): # Remove stopwors in a text
+#     listStopwords = set(stopwords.words('indonesian'))
+#     filtered = []
+#     for txt in text:
+#         if txt not in listStopwords:
+#             filtered.append(txt)
+#     text = filtered 
+#     return text
 
-def stemmingText(text): # Reducing a word to its word stem that affixes to suffixes and prefixes or to the roots of words
-    factory = StemmerFactory()
-    stemmer = factory.create_stemmer()
-    text = [stemmer.stem(word) for word in text]
-    return text
+# def stemmingText(text): # Reducing a word to its word stem that affixes to suffixes and prefixes or to the roots of words
+#     factory = StemmerFactory()
+#     stemmer = factory.create_stemmer()
+#     text = [stemmer.stem(word) for word in text]
+#     return text
 
-def toSentence(list_words): # Convert list of words into sentence
-    sentence = ' '.join(word for word in list_words)
-    return sentence
+# def toSentence(list_words): # Convert list of words into sentence
+#     sentence = ' '.join(word for word in list_words)
+#     return sentence
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -106,7 +106,7 @@ def predict():
         new_corpus = [new_review]
         new_X_test = cv.transform(new_corpus).toarray()
         pred = model.predict(new_X_test)
-          return render_template('result.html',prediction=pred)
+        return render_template('result.html',prediction=pred)
 
 if __name__ == "__main__":
     app.run(debug=True)    
